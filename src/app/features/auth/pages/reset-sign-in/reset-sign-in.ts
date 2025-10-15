d1fd90300c05dbde9b6daa3da63f412f3b0377cc
Abduqodirov <abduqodirovsarvar.2002@gmail.com>
2025-10-15 11:02:04 +0500

    add some changes

---
+++ src/app/features/auth/pages/reset-sign-in/reset-sign-in.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-reset-sign-in',
+  imports: [],
+  templateUrl: './reset-sign-in.html',
+  styleUrl: './reset-sign-in.scss'
+})
+export class ResetSignIn {
+
+}
